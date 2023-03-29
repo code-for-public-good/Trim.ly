7f88575872035506cbb347c1bff05a8966c71425
Jcheez <jiajunlee2@gmail.com>
2023-03-30 00:06:58 +0800

    initial init

---
+++ api/README.md
@@ -0,0 +1,8 @@
+## Introduction
+Backend service of Trim.ly
+
+## Running a local instance
+```
+npm run build
+npm start
+```
---
+++ api/index.ts
@@ -0,0 +1,23 @@
+import express, { Express } from 'express'
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config()
+
+const app: Express = express()
+const PORT = process.env.PORT || "3000"
+
+// Connection to MongoDB
+const DB_URL = process.env.DB_URL
+mongoose.connect(`${DB_URL}`).then(() => {
+    console.log(`Connection established with DB!`)
+}).catch(err => {
+    console.log(`Error connecting to DB: ${err}`)
+})
+
+// Starting the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+}).on("error", err => {
+    console.log(`Server is not running. ${err}`)
+})
