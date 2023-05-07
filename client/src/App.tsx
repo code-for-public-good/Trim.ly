6a4f04e23dc28113daff40b5f5ebf51eaee4a0de
Jia Jun Lee <65590028+Jcheez@users.noreply.github.com>
2023-05-08 00:29:10 +0800

    NavBar implemention (#5)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import FeaturesPage from './pages/FeaturesPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+					<Navbar></Navbar>
+					<Routes>
+						<Route path='/' element={<HomePage />}/>
+						<Route path='/features' element={<FeaturesPage />}/>
+						<Route path='/about' element={<AboutPage />}/>
+					</Routes>
+				</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,125 @@
+import {
+    AppBar, 
+    Box, 
+    Toolbar, 
+    Button, 
+    Container, 
+    Stack, 
+    Link 
+} from '@mui/material'
+import { useLocation } from "react-router-dom"
+import React from 'react'
+
+const navBarDotTransparent = {
+    'display': 'inline-block',
+    'content': '""',
+    'borderRadius': '50%',
+    'position': 'relative',
+    'width': '6px',
+    'height': '6px',
+    'left': '-6px',
+    'bottom': '2px',
+    'backgroundColor': 'white'
+}
+
+const navBarDotColored = {
+    'display': 'inline-block',
+    'content': '""',
+    'borderRadius': '50%',
+    'position': 'relative',
+    'width': '6px',
+    'height': '6px',
+    'left': '-6px',
+    'bottom': '2px',
+    'backgroundColor': '#8191c9'
+}
+
+export default function Navbar() {
+    
+    const location = useLocation();
+    
+    return (
+        <AppBar
+            color='default'
+            elevation={0}
+            sx={{ height: '100px' }}
+            position='static'
+        >
+            <Toolbar sx={{ height: '100%' }}>
+                <Container sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, height: '100%' }}>
+                    <Box display={'flex'} alignItems={'center'} mb={0.5}>
+                        <Link href='/' underline='none' sx={{ 'color': 'black', 'fontSize': '24px', }}>
+                            TRIM.LY
+                        </Link>
+                    </Box>
+                    <Stack
+                        direction={'row'}
+                        component={'nav'}
+                        spacing={5}
+                        ml={5}
+                        mb={1}
+                        alignItems={'center'}
+                    >
+                        <Link
+                            href='/'
+                            underline='none'
+                            sx={{
+                                'color': 'black',
+                                'fontSize': '1rem',
+                                '::before': location.pathname === '/' ? navBarDotColored : navBarDotTransparent,
+                                ':hover': {
+                                    '::before': navBarDotColored
+                                }
+                            }}
+                        >
+                            Home
+                        </Link>
+                        <Link
+                            href='/features'
+                            underline='none'
+                            sx={{
+                                'color': 'black',
+                                'fontSize': '1rem',
+                                '::before': location.pathname === '/features' ? navBarDotColored : navBarDotTransparent,
+                                ':hover': {
+                                    '::before': navBarDotColored
+                                }
+                            }}
+                        >
+                            Features
+                        </Link>
+                        <Link
+                            href='/about'
+                            underline='none'
+                            marginLeft={10}
+                            sx={{
+                                'color': 'black',
+                                'fontSize': '1rem',
+                                '::before': location.pathname === '/about' ? navBarDotColored : navBarDotTransparent,
+                                ':hover': {
+                                    '::before': navBarDotColored
+                                }
+                            }}
+                        >
+                            About
+                        </Link>
+                    </Stack>
+                    <Stack
+                        direction={'row'}
+                        spacing={3}
+                        flexGrow={1}
+                        justifyContent={'flex-end'}
+                        alignItems={'center'}
+                    >
+                        <Link href='/' underline='none' sx={{ 'color': 'black', 'fontSize': '1rem' }}>
+                            Signin
+                        </Link>
+                        <Button variant='contained' size='large' sx={{ backgroundColor: '#8191c9', ":hover": { backgroundColor: '#636da3' } }}>
+                            Signup
+                        </Button>
+                    </Stack>
+                </Container>
+            </Toolbar>
+        </AppBar>
+    )
+}
