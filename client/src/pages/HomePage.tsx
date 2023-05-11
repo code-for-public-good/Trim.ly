f522cca5ff1b610415609a12d9c0d865e2aa7a9f
Jcheez <jiajunlee2@gmail.com>
2023-05-11 13:25:25 +0800

    Header and subheader completed

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,21 @@
+import {
+	Box, Typography,
+} from '@mui/material'
 import React from 'react'
 
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+	return (
+		<Box display={'flex'} height={'calc(100vh - 100px)'}>
+			<Box>
+				<Typography variant='h1' fontSize={'2rem'} fontWeight={700} color={'#433f8f'}>
+					Generate Custom Short Links!
+				</Typography>
+				<Typography variant='body1' fontSize={'1rem'}>
+					Trim.ly is a url shortener that allows you to create short links
+					from those long web addresses. It allows you to engage with your customers.
+					Get Started now!
+				</Typography>
+			</Box>
+		</Box>
+	)
 }
