a9bb2bc023ecb8c94377a612195699439fa346da
Jcheez <jiajunlee2@gmail.com>
2023-07-30 13:22:17 +0800

    Link Gen template

--- client/src/pages/LinkGeneratorPage.tsx
+++ client/src/pages/LinkGeneratorPage.tsx
@@ -1,7 +1,23 @@
+import { Box, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 export default function LinkGeneratorPage() {
-  return (
-    <div>LinkGeneratorPage</div>
-  )
+    return (
+        <Box display={'flex'} flexDirection={'column'} height={"100%"} alignItems={'center'} justifyContent={'center'} sx={{ backgroundColor: "rgb(255 255 255)" }}>
+            <Box width={'100%'}>
+                <Typography
+                    variant={'h4'}
+                    textAlign={'left'}
+                    width={'100%'}
+                    fontWeight={'bold'}
+                    mb={5}
+                >
+                    Link Generator
+                </Typography>
+                <Stack spacing={5} alignItems={'flex-start'}>
+                    <TextField fullWidth/>
+                </Stack>
+            </Box>
+        </Box>
+    )
 }
